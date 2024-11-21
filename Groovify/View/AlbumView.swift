86e92a4889185bc66ec8144429698c3f13ce87ba
Kunal <bajajku@sheridancollege.ca>
2024-11-21 01:52:30 -0500

    Created view to navigate to and display album when clicked on it only working for New releases for now.

---
+++ Groovify/View/AlbumView.swift
@@ -0,0 +1,18 @@
+//
+//  AlbumView.swift
+//  Groovify
+//
+//  Created by Kunal Bajaj on 2024-11-21.
+//
+
+import SwiftUI
+
+struct AlbumView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlbumView()
+}
