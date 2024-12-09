f177b516810b7b2ad9d1754f4e4bbcdd7b83721f
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2024-12-09 01:41:42 -0500

    Updated User entity and added user profile.

--- Groovify/Models/User/User.swift
+++ Groovify/Models/User/User.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-
+import CoreData
 
 /// This user model represents the user progamatically. The "MOCKUSER" extension, represents a fake user to be tested in code
 struct User: Identifiable, Codable{
@@ -25,7 +25,28 @@ struct User: Identifiable, Codable{
         return ""
     }
 }
+///maps between UserEntity (Core Data object) and the User struct. This decouples the Core Data layer from the rest of the app.
+extension UserEntity {
+    func toUser() -> User {
+        return User(
+            id: self.id ?? "",
+            fullname: self.fullname ?? "",
+            email: self.email ?? ""
+        )
+    }
 
+    func update(from user: User, context: NSManagedObjectContext) {
+        self.id = user.id
+        self.fullname = user.fullname
+        self.email = user.email
+
+        do {
+            try context.save()
+        } catch {
+            print("Failed to save UserEntity: \(error.localizedDescription)")
+        }
+    }
+}
 extension User{
     //test user
     static var MOCK_USER = User(id: NSUUID().uuidString, fullname: "Daniel Ek", email: "tigerwoods@tigerwoods.com")
